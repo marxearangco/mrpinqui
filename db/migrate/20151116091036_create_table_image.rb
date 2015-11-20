157914d02ca3af851488c0fe0e15f300cfb8ba94
marxearangco <marcoarangco@gmail.com>
2015-11-20 14:04:05 +0800

    added initial login

--- db/migrate/20151116091036_create_table_image.rb
+++ db/migrate/20151116091036_create_table_image.rb
@@ -5,5 +5,16 @@ class CreateTableImage < ActiveRecord::Migration
       t.attachment :photo
       t.timestamps
     end
+
+    create_table "tblempauth", force: true do |t|
+	  t.column "idEmp", :integer
+	  t.column  "userName", :string
+	  t.column  "passWord", :string
+	  t.column "privilege", :integer
+	end
+
+	create_table "tblprivilege", force: true do |t|
+	  t.column "privilege", :string
+	end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-  create_table "tblempauth", id: false, force: true do |t|
-    t.integer "id",                   null: false
-    t.integer "idEmp"
-    t.string  "userName",  limit: 15
-    t.string  "passWord",  limit: 50
-    t.integer "privilege"
-  end
-
-  create_table "tblprivilege", id: false, force: true do |t|
-    t.integer "id",                   null: false
-    t.string "privilege"
-  end
-
   connection=ActiveRecord::Base.connection
   connection.execute('insert into "tblempauth"("idEmp","userName","passWord","priviledge" values(1,"administrator","p@$$w0rd",1)')
-  connection.execute('insert into "tblprivilege"("id","priviledge") values(1,"administrator")')  	
+  connection.execute('insert into "tblprivilege"("id","priviledge") values(1,"administrator")')
